3b151446f8acd2a35bf99e3f15f08c04a293b026
JoseRamirez21 <josexitorap@gmail.com>
2024-10-10 09:48:14 -0500

    1

--- views/login.php
+++ views/login.php
@@ -1,53 +1,8 @@
-<main>
-    
-    <div class="container" id="container">
-        <div class="form-container sign-up">
-            <form>
-                <h1>Crear Usuario</h1>
-                <div class="social-icons">
-                    <a href="https://web.whatsapp.com/" class="icon"><i class="fa-brands fa-whatsapp"></i></a>
-                    <a href="https://www.instagram.com/josec_ramirezramos/" class="icon"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="https://mail.google.com/mail/u/0/#inbox" class="icon"><i class="fa-brands fa-google"></i></a>
-                    <a href="https://www.facebook.com/josec.ramirez.718" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                </div>
-                <span>Puede utilizar su correo electronico</span>
-                <input type="text" placeholder="Nombre">
-                <input type="email" placeholder="Correo Electronico">
-                <input type="password" placeholder="Contraseña">
-                <button><a href="<?php echo BASE_URL; ?>inicio" style="color:white;">Registrarse <i class="fa fa-user"></i></a></button>
-            </form>
-        </div>
-        <div class="form-container sign-in">
-            <form>
-                <h1>Iniciar Sesión</h1>
-                <div class="social-icons">
-                    <a href="https://web.whatsapp.com/" class="icon"><i class="fa-brands fa-whatsapp"></i></a>
-                    <a href="https://www.instagram.com/josec_ramirezramos/" class="icon"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="https://mail.google.com/mail/u/0/#inbox" class="icon"><i class="fa-brands fa-google"></i></a>
-                    <a href="https://www.facebook.com/josec.ramirez.718" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                </div>
-                <span>Inicie sesión con su correo electrónico</span>
-                <input type="email" placeholder="Correo Electronico">
-                <input type="password" placeholder="Contraseña">
-                <a href="#">¿Olvidaste tu contraseña?</a>
-                <button><a href="<?php echo BASE_URL; ?>inicio" style="color:white;"> Iniciar Sesión <i class="fa fa-user"></i></a></button>
-            </form>
-        </div>
-        <div class="toggle-container">
-            <div class="toggle">
-                <div class="toggle-panel toggle-left">
-                    <h1>¡Bienvenido de nuevo!</h1>
-                    <p>Ingrese sus datos y disfrute de nuestra Pagina Web</p>
-                    <button class="hidden" id="login">Iniciar Sesión</button>
-                </div>
-                <div class="toggle-panel toggle-right">
-                    <h1>Hola!</h1>
-                    <p>Regístrese con sus datos personales para poder comprar y ver las grandes promociones </p>
-                    <button class="hidden" id="register">Registrarse</button>
-                </div>
-            </div>
-        </div>
+<div class="container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcOg8Ax5g9goNiTXPtuLwE23JPqOtrO3VSEQ&s" alt="JC Vinos y Licores" class="img-fluid" style="max-width: 150px; margin: 20px;">
+        <h1>BIENVENIDOS</h1>
+        <p style="font-size: 28px;">Para continuar debes de ser mayor de 18 años</p>
+        <a href="pages/pagina_principal.html" class="btn btn-success" style="font-size: 22px;">Soy mayor de 18 años</a>
+        <a href="#" class="btn btn-danger" style="font-size: 22px;">Soy menor de 18 años</a><br><br>
+        <p style="font-size: 22px;">NO COMPARTA ESTE CONTENIDO CON MENORES DE EDAD</p>
     </div>
-
-
-</main>
