ca25e4797c3ce20e8e2fa319ddb2f2cdd39d5b47
MaribelVilca <maribelvilcatincopa3@gmail.com>
2024-11-05 09:51:40 -0500

    a

--- views/include/footer.php
+++ views/include/footer.php
@@ -84,6 +84,12 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- <script
+     src="https://code.jquery.com/jquery-3.7.1.js"
+     integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+     crossorigin="anonymous"></script> -->
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>
