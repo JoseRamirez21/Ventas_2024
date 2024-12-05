6e5ffef307f6cfc5a4aaae30630f97064a01044c
JoseRamirez21 <josexitorap@gmail.com>
2024-12-04 21:24:19 -0500

    bd

--- database/licoreria_bd.sql
+++ database/licoreria_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-12-2024 a las 23:39:29
+-- Tiempo de generación: 05-12-2024 a las 03:23:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
